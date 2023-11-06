2e45e33500ff085090d7398a246d831dd470357c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-11-06 15:53:46 -0800

    update part1

--- project2/part1/part1.py
+++ project2/part1/part1.py
@@ -8,10 +8,16 @@ from mininet.cli import CLI
 
 class part1_topo(Topo):
     def build(self):
-        pass
-        # switch1 = self.addSwitch('switchname')
-        # host1 = self.addHost('hostname')
-        # self.addLink(hostname,switchname)
+        switch1 = self.addSwitch('s1')
+        host1 = self.addHost('h1')
+        host2 = self.addHost('h2')
+        host3 = self.addHost('h3')
+        host4 = self.addHost('h4')
+
+        self.addLink(host1, switch1)
+        self.addLink(host2, switch1)
+        self.addLink(host3, switch1)
+        self.addLink(host4, switch1)
 
 
 topos = {"part1": part1_topo}
